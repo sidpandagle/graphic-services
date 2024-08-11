c0b449689a979109017d3122fd2259d3b1bcaa6e
Siddhant Pandagle <siddhant.pandagle1998@gmail.com>
2024-08-11 10:26:42 +0000

    Label bug fix

--- shared/components/CTA.tsx
+++ shared/components/CTA.tsx
@@ -8,15 +8,15 @@ export default function CTA() {
                 <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
                 <form action="#" className="space-y-8">
                     <div>
-                        <label for="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
+                        <label className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
                         <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" placeholder="name@flowbite.com" required/>
                     </div>
                     <div>
-                        <label for="subject" className="block mb-2 text-sm font-medium text-gray-900">Subject</label>
+                        <label className="block mb-2 text-sm font-medium text-gray-900">Subject</label>
                         <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500" placeholder="Let us know how we can help you" required/>
                     </div>
                     <div className="sm:col-span-2">
-                        <label for="message" className="block mb-2 text-sm font-medium text-gray-900 ">Your message</label>
+                        <label className="block mb-2 text-sm font-medium text-gray-900 ">Your message</label>
                         <textarea id="message" rows={6} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500" placeholder="Leave a comment..."></textarea>
                     </div>
                     <button className="btn btn-neutral">Send Message</button>
