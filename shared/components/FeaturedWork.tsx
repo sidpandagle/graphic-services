078d0f907fc11616c69c23e3bf76317ed7e96503
fudge-fantastic <adi.pandagle@gmail.com>
2025-07-08 21:24:01 +0530

    Header Changes

--- shared/components/FeaturedWork.tsx
+++ shared/components/FeaturedWork.tsx
@@ -2,16 +2,17 @@ import Image from "next/image";
 
 export default function FeaturedWork() {
     return (
-        <div className="p-8  text-center" id="featured-work">
+        <div className="p-8 text-center container mx-auto" id="featured-work">
             <div className="mb-4 lg:mb-8 text-center">
                 <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">Featured Work</h2>
                 <p className="text-gray-500 sm:text-xl dark:text-gray-400">Explore a collection of graphic designs that transform concepts into captivating visuals, showcasing creativity, originality, and the power of design to tell stories.</p>
             </div>
 
             <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-                {[...(new Array(16))].map((x, index) => (
-                   <div className="h-60 w-full overflow-hidden rounded-lg">
-                        <img className="h-60 w-full object-cover duration-200 hover:scale-110" src={`/work-images/${Math.ceil(Math.random() * 4)}.png`} alt="" />
+                {[...(new Array(2))].map((x, index) => (
+                   <div className="h-40 w-full overflow-hidden rounded-lg">
+                        {/* <img className="h-40 w-full object-cover duration-200 hover:scale-110" src={`/work-images/${Math.ceil(Math.random() * 4)}.png`} alt="" /> */}
+                        <img className="h-40 w-full object-cover duration-200 hover:scale-110" src={`/work-images/4.png`} alt="" />
                     </div> 
                 ))}
             </div>
--- shared/components/NavBar.tsx
+++ shared/components/NavBar.tsx
@@ -3,18 +3,22 @@ import Image from "next/image";
 export default function NavBar() {
     return (
         <div className="border-b">
-            <div id="navbar" className="container mx-auto p-4 md:p-8 flex gap-8 justify-between items-center">
-                <div>PixelForgeStudio</div>
-                <div className="hidden md:flex gap-8">
+            <div id="navbar" className="container mx-auto p-4 md:p-6 flex gap-8 justify-between items-end">
+                <div className="flex">
+                    {/* <p>PixelForgeStudio</p> */}
+                    <img  src="/Pixelforgestudio-logo.svg" alt="logo" className="h-10 w-22" />
+                </div>
+                <div className="hidden md:flex gap-8 text-sm">
                     <a href="/#services" className="cursor-pointer">Services</a>
                     <a href="/#featured-work" className="cursor-pointer">Featured Work</a>
                     <a href="/#about-us" className="cursor-pointer">About Us</a>
                     {/* <a href="/#pricing" className="cursor-pointer">Pricing</a> */}
+                    <a href="/#contact-us" className="cursor-pointer">Contact Us</a>
                 </div>
-                <a href="/#contact-us" className="hidden md:flex cursor-pointer">Contact Us</a>
+                {/* <a href="/#contact-us" className="hidden md:flex cursor-pointer  text-sm">Contact Us</a> */}
                 <div className="dropdown dropdown-end md:hidden z-20">
                     <div tabIndex={0} role="button" className="btn m-1">///</div>
-                    <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                    <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow ">
                         <li><a href="/#services" className="cursor-pointer">Services</a></li>
                         <li><a href="/#featured-work" className="cursor-pointer">Featured Work</a></li>
                         <li><a href="/#about-us" className="cursor-pointer">About Us</a></li>
